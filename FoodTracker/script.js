268f87b79b64c9ad04944bba6d52c9728383ca99
Ken MacLeod <mqbitsko@gmail.com>
2018-07-30 19:41:21 -0500

    snapshot

--- FoodTracker/script.js
+++ FoodTracker/script.js
@@ -47,7 +47,13 @@ function showMessage(message) {
   });
 }
 
-// TODO: Add Google Sign-in.
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
 
 
 
